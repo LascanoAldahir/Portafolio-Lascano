b551cc23248853e422179cf9552cda29a3aa8359
LAPTOP-M8V9U3AJ\davic <davicho.DL1@gmail.com>
2023-12-27 22:23:10 -0500

    Segundo metodo atlas

--- src/database.js
+++ src/database.js
@@ -1,22 +1,25 @@
-// IMPORTAR MONGOOSE
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-// CADENA DE CONEXION A LA BDD
-//const MONGODB_URI = 'mongodb+srv://byrontosh:sistemas@cluster0.6e8zntc.mongodb.net/test'
+//Se Establece la cadena de conexion que proporciona Mongo Atlas
+//mongodb+srv://Dennis2003:<password>@cluster0.euw1ldq.mongodb.net/
 
-// CREAR UN METODO PARA HACER LA CADENA DE CONEXION
 
-connection = async()=>{
-try {
-// INVOCAR EL METODO CONNECT
-await mongoose.connect(process.env.MONGODB_URI)
-// RESPUESTA DE LA PROMESA == "ERROR"
-console.log("Database is connected")
-} catch (error) {
-//RESPUESTA DE LA PROMESA == "ERROR"
+//Se Establece la cadena de conexion que proporciona Mongo Atlas
+//mongodb+srv://Dennis2003:<password>@cluster0.euw1ldq.mongodb.net/
+//Se cambia <password> por la contraseña que se usara ademas de agregar al final el nombre de la BD
+const MONGODB_URI = 'mongodb+srv://AldahirL:Kamekameha1@aldahircluster.eczbwcw.mongodb.net/';
 
-console.log(error);
-}
-}
 
-module.exports = connection
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database is connected");
+    } catch (error) {
+        console.error("Error connecting to database:", error);
+    }
+};
+
+module.exports = connectDB;
